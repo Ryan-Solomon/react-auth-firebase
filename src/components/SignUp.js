50435fb0d7acf29bc4e1a71fbcf56039e6b06f45
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-06 12:32:04 -0600

    add form

---
+++ src/components/SignUp.js
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react';
+
+const SignUp = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  return (
+    <main className='form-container'>
+      <form>
+        <header>
+          <h1>Sign Up</h1>
+        </header>
+        <section className='email'>
+          <label htmlFor='email'>Email</label>
+          <input required ref={emailRef} id='email' type='email' />
+        </section>
+        <section className='password'>
+          <label htmlFor='password'>Password</label>
+          <input required ref={passwordRef} id='password' type='email' />
+        </section>
+        <section className='password-confirm'>
+          <label htmlFor='password-confirm'>Confirm Password</label>
+          <input
+            required
+            ref={passwordConfirmRef}
+            id='password-confirm'
+            type='password'
+          />
+        </section>
+      </form>
+    </main>
+  );
+};
+
+export default SignUp;
